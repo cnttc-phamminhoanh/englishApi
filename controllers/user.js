6ce584ece3f011a86758fdc00c39671174fd0f48
unknown <oanhpham41121@gmail.com>
2021-09-11 17:36:55 +0700

    fix conflic

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,7 @@
 const User = require('../models/User');
 
 const index = (req, res, next) => {
-    User.find({}).then((users) => {
-        return res.status(200).json({
-            message: users
-        });
-    });
+    
 }
 
 module.exports = {
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,4 @@ const express = require('express');
 
 const userController = require('../controllers/user');
 
-const router = express.Router();
-
-router.get('/',userController.index);
-
 module.exports = router;
